391d8fe2201241b885644ff3e67b19012b11a4be
sagarbhade <bpsaagar@gmail.com>
2023-01-16 23:03:10 +0530

    mvc app

---
+++ Ecommerce/BLL/CatalogManager.cs
@@ -0,0 +1,13 @@
+﻿namespace BLL;
+
+using BOL;
+using DAL;
+
+public class CatalogManager
+{
+    public List<Product> GetAllProducts(){
+        List<Product> allProducts=new List<Product>();
+        allProducts=DBManager.GetAllProducts();
+        return allProducts;
+    }
+}
---
+++ Ecommerce/EStore/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EStore.Models;
+using BOL;
+
+
+namespace EStore.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+    //Get All products from BLL
+    //bind all products to viewdata
+    //send viewData to View
+
+    public IActionResult Index()
+    {
+        List<Product> allProducts=new List<Product>();
+         this.ViewData["products"]=allProducts;
+        return View();
+    }
+
+    public IActionResult Details(int id)
+    {   
+       
+        return View();
+    }
+
+    public IActionResult Insert()
+    {
+        return View();
+    }
+
+    public IActionResult Delete(int id)
+    {
+        //return View();
+        return RedirectToAction("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
